606dd580df0fbae6beab8efdd597a60292a16873
Nathaniel Tooley <nathanieltooley24@gmail.com>
2022-01-28 16:00:07 -0600

    added break functionality and data map

--- src/com/nathanieltooley/mips/Main.java
+++ src/com/nathanieltooley/mips/Main.java
@@ -3,12 +3,18 @@ package com.nathanieltooley.mips;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     private static final int MAX_REGISTERS = 32;
 
     public static int[] registers = new int[MAX_REGISTERS];
+    public static Map<Integer, Integer> data = new HashMap<>();
+    public static ArrayList<Instruction> instructions = new ArrayList<>();
 
     public static void main(String[] args) {
         byte[] bytes = readBinaryFile(args[0]);
@@ -16,45 +22,55 @@ public class Main {
 
         String[] bytes32 = getBytesAs32Bits(bytes);
 
+        boolean reachedBreak = false;
+
         for (String word : bytes32) {
-            Instruction inst = new Instruction(word);
-            printMipsCommand(inst.sepStrings);
-            System.out.printf(" %s\t", memoryAddress);
+            if (reachedBreak){
+                int dataValue = Instruction.binToDec(word, true);
+                System.out.printf("%s\t    %s\t %s", word, memoryAddress, dataValue);
 
-            if (inst.valid == 0) {
-                System.out.print(" Invalid Instruction");
-            }
-            else if (inst.opcode == 40) {
-                System.out.printf(" ADDI\t R%s, R%s, #%s", inst.rt, inst.rs, inst.immd);
-            }
-            else if (inst.opcode == 43) {
-                System.out.printf(" SW  \t R%s, %s(R%s)", inst.rt, inst.immd, inst.rs);
-            }
-            else if (inst.opcode == 32 && inst.func == 0) {
-                // SLL Command
-                System.out.printf(" SLL\t R%s, R%s, #%s", inst.rd, inst.rt, inst.sa);
-                //SLL	R10, R1, #2
-            }
-            else if (inst.opcode == 32 && inst.func == 34){
-                System.out.printf(" SUB \t R%s, R%s, R%s", inst.rd, inst.rs, inst.rt);
-            }
-            else if (inst.opcode == 32 && inst.func == 32){
-                System.out.printf(" ADD \t R%s, R%s, R%s", inst.rd, inst.rs, inst.rt);
-            }
-            else if (inst.opcode == 35) {
-                System.out.printf(" LW  \t R%s, %s(R%s)", inst.rt, inst.immd, inst.rs);
-            }
-            else if (inst.opcode == 34) {
-                System.out.printf(" J  \t #%s", inst.j);
-            }
-            else if (inst.opcode == 33) {
-                System.out.printf(" BLTZ  \t R%s, #%s", inst.rs, inst.immd);
-            }
-            else if (inst.opcode == 32 && inst.func == 13){
-                System.out.printf(" BREAK");
-            }
+                data.put(memoryAddress, dataValue);
+            } else {
+                Instruction inst = new Instruction(word);
+                printMipsCommand(inst.sepStrings);
+                System.out.printf(" %s\t", memoryAddress);
 
+                if (inst.valid == 0) {
+                    System.out.print(" Invalid Instruction");
+                }
+                else if (inst.opcode == 40) {
+                    System.out.printf(" ADDI\t R%s, R%s, #%s", inst.rt, inst.rs, inst.immd);
+                }
+                else if (inst.opcode == 43) {
+                    System.out.printf(" SW  \t R%s, %s(R%s)", inst.rt, inst.immd, inst.rs);
+                }
+                else if (inst.opcode == 32 && inst.func == 0) {
+                    // SLL Command
+                    System.out.printf(" SLL\t R%s, R%s, #%s", inst.rd, inst.rt, inst.sa);
+                    //SLL	R10, R1, #2
+                }
+                else if (inst.opcode == 32 && inst.func == 34){
+                    System.out.printf(" SUB \t R%s, R%s, R%s", inst.rd, inst.rs, inst.rt);
+                }
+                else if (inst.opcode == 32 && inst.func == 32){
+                    System.out.printf(" ADD \t R%s, R%s, R%s", inst.rd, inst.rs, inst.rt);
+                }
+                else if (inst.opcode == 35) {
+                    System.out.printf(" LW  \t R%s, %s(R%s)", inst.rt, inst.immd, inst.rs);
+                }
+                else if (inst.opcode == 34) {
+                    System.out.printf(" J  \t #%s", inst.j);
+                }
+                else if (inst.opcode == 33) {
+                    System.out.printf(" BLTZ  \t R%s, #%s", inst.rs, inst.immd);
+                }
+                else if (inst.opcode == 32 && inst.func == 13){
+                    System.out.printf(" BREAK");
+                    reachedBreak = true;
+                }
 
+                instructions.add(inst);
+            }
 
             System.out.println();
             memoryAddress += 4;

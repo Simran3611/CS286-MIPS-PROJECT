fcf0b74c6708065e834b7f9dd366dbc7dd0c1fae
Simran3611 <98481310+Simran3611@users.noreply.github.com>
2022-01-28 14:24:27 -0600

    Update Main.java
    I have finished up the SLL, also added ADD, LW, J, & BLTZ. I think that it should cover everything if I am not missing anything.

--- src/com/nathanieltooley/mips/Main.java
+++ src/com/nathanieltooley/mips/Main.java
@@ -37,10 +37,25 @@ public class Main {
             }
             else if (inst.opcode == 32 && inst.func == 0) {
                 // SLL Command
+                System.out.printf(" SLL\t R%s, R%s, #%s", inst.rd, inst.rt, inst.immd);
+                //SLL	R10, R1, #2
             }
             else if (inst.opcode == 32 && inst.func == 34){
                 System.out.printf(" SUB \t R%s, R%s, R%s", inst.rd, inst.rs, inst.rt);
             }
+            else if (inst.opcode == 32 && inst.func == 32){
+                System.out.printf(" ADD \t R%s, R%s, R%s", inst.rd, inst.rs, inst.rt);
+            }
+            else if (inst.opcode == 35) {
+                System.out.printf(" LW  \t R%s, %s(R%s)", inst.rt, inst.immd, inst.rs);
+            }
+            else if (inst.opcode == 2) {
+                System.out.printf(" J  \t #%s", inst.immd);
+            }
+            else if (inst.opcode == 7) {
+                System.out.printf(" BLTZ  \t R%s, #%s", inst.rs, inst.immd);
+            }
+
 
 
             System.out.println();
